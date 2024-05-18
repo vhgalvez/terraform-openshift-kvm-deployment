29e59df9d88cce4e428a688b4a65de593b3b0f06
Victor Galvez <vhgalvez@gmail.com>
2024-05-18 23:40:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = "= 1.8.3"
+
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
@@ -12,28 +13,14 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-module "network_br0" {
-  source                    = "./network_br0"
-  cluster_name              = var.cluster_name
-  rocky9_image              = var.rocky9_image
-  ssh_keys                  = var.ssh_keys
-  vm_rockylinux_definitions = var.vm_rockylinux_definitions_br0
+module "bastion_network" {
+  source = "./bastion_network"
 }
 
-module "network_kube_02" {
-  source                    = "./network_kube_02"
-  depends_on                = [module.network_br0]
-  cluster_name              = var.cluster_name
-  rocky9_image              = var.rocky9_image
-  ssh_keys                  = var.ssh_keys
-  vm_rockylinux_definitions = var.vm_rockylinux_definitions_kube_02
+module "nat_network_02" {
+  source = "./nat_network_02"
 }
 
-module "network_kube_03" {
-  source         = "./network_kube_03"
-  depends_on     = [module.network_kube_02]
-  cluster_name   = var.cluster_name
-  base_image     = var.base_image
-  ssh_keys       = var.ssh_keys
-  vm_definitions = var.vm_definitions_kube_03
+module "nat_network_03" {
+  source = "./nat_network_03"
 }
