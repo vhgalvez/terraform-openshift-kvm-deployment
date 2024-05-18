0fc6dc4dc1aa193e5ed8c742af869ab33ce70359
Victor Galvez <vhgalvez@gmail.com>
2024-05-19 01:50:30 +0200

    Update main.tf

--- nat_network_03/main.tf
+++ nat_network_03/main.tf
@@ -1,8 +1,11 @@
+# nat_network_03/main.tf
 terraform {
+  required_version = ">= 0.13"
+
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.7.1"
+      version = "0.7.0"
     }
     ct = {
       source  = "poseidon/ct"
@@ -25,41 +28,81 @@ resource "libvirt_network" "kube_network_03" {
   addresses = ["10.17.4.0/24"]
 }
 
-resource "libvirt_pool" "volumetmp_nat_03" {
-  name = "${var.cluster_name}_nat_03"
+resource "libvirt_pool" "volumetmp_03" {
+  name = "${var.cluster_name}_03"
   type = "dir"
-  path = "/var/lib/libvirt/images/${var.cluster_name}_nat_03"
+  path = "/var/lib/libvirt/images/${var.cluster_name}_03"
 }
 
 resource "libvirt_volume" "base" {
-  name   = "${var.cluster_name}_base"
+  name   = "${var.cluster_name}-base"
   source = var.base_image
-  pool   = libvirt_pool.volumetmp_nat_03.name
+  pool   = libvirt_pool.volumetmp_03.name
   format = "qcow2"
 }
 
-# Otras configuraciones específicas para nat_network_03
+data "template_file" "vm-configs" {
+  for_each = var.vm_definitions
 
-# Ejemplo de dominio
-resource "libvirt_domain" "vm_nat_03" {
-  name   = "bootstrap1"
-  memory = 1024
-  vcpu   = 1
+  template = file("${path.module}/configs/machine-${each.key}-config.yaml.tmpl")
+
+  vars = {
+    ssh_keys     = jsonencode(var.ssh_keys),
+    name         = each.key,
+    host_name    = "${each.key}.${var.cluster_name}.${var.cluster_domain}",
+    strict       = true,
+    pretty_print = true
+  }
+}
+
+data "ct_config" "vm-ignitions" {
+  for_each = var.vm_definitions
+
+  content = data.template_file.vm-configs[each.key].rendered
+}
+
+resource "libvirt_ignition" "ignition" {
+  for_each = var.vm_definitions
+
+  name    = "${each.key}-ignition"
+  pool    = libvirt_pool.volumetmp_03.name
+  content = data.ct_config.vm-ignitions[each.key].rendered
+}
+
+resource "libvirt_volume" "vm_disk" {
+  for_each = var.vm_definitions
+
+  name           = "${each.key}-${var.cluster_name}.qcow2"
+  base_volume_id = libvirt_volume.base.id
+  pool           = libvirt_pool.volumetmp_03.name
+  format         = "qcow2"
+}
+
+resource "libvirt_domain" "machine" {
+  for_each = var.vm_definitions
+
+  name   = each.key
+  vcpu   = each.value.cpus
+  memory = each.value.memory
 
   network_interface {
     network_id     = libvirt_network.kube_network_03.id
     wait_for_lease = true
-    addresses      = ["10.17.4.20"]
+    addresses      = [each.value.ip]
   }
 
   disk {
-    volume_id = libvirt_volume.base.id
+    volume_id = libvirt_volume.vm_disk[each.key].id
   }
+
+  coreos_ignition = libvirt_ignition.ignition[each.key].id
 
   graphics {
     type        = "vnc"
     listen_type = "address"
   }
+}
 
-  coreos_ignition = libvirt_ignition.bootstrap1.id
+output "ip_addresses" {
+  value = { for key, machine in libvirt_domain.machine : key => machine.network_interface[0].addresses[0] if length(machine.network_interface[0].addresses) > 0 }
 }
