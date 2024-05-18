56cd58fcf258064d3bc34ceb6dc317cdd884c210
Victor Galvez <vhgalvez@gmail.com>
2024-05-18 23:48:19 +0200

    update

--- nat_network_02/main.tf
+++ nat_network_02/main.tf
@@ -1,32 +1,20 @@
-terraform {
-  required_providers {
-    libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "0.7.1"
-    }
-  }
-}
-
-provider "libvirt" {
-  uri = "qemu:///system"
-}
-
+# nat_network_02/main.tf
 resource "libvirt_network" "kube_network_02" {
-  name      = var.rocky9_network_name
+  name      = "kube_network_02"
   mode      = "nat"
   addresses = ["10.17.3.0/24"]
 }
 
-resource "libvirt_pool" "volumetmp_kube_02" {
-  name = "${var.cluster_name}_kube_02"
+resource "libvirt_pool" "volumetmp" {
+  name = var.cluster_name
   type = "dir"
-  path = "/var/lib/libvirt/images/${var.cluster_name}_kube_02"
+  path = "/var/lib/libvirt/images/${var.cluster_name}"
 }
 
 resource "libvirt_volume" "rocky9_image" {
-  name   = "${var.cluster_name}_kube_02-rocky9_image"
+  name   = "${var.cluster_name}-rocky9_image"
   source = var.rocky9_image
-  pool   = libvirt_pool.volumetmp_kube_02.name
+  pool   = libvirt_pool.volumetmp.name
   format = "qcow2"
 }
 
@@ -35,8 +23,8 @@ data "template_file" "vm-configs" {
 
   template = file("${path.module}/config/${each.key}-user-data.tpl")
   vars = {
-    ssh_keys = jsonencode(var.ssh_keys)
-    hostname = each.key
+    ssh_keys = jsonencode(var.ssh_keys),
+    hostname = each.key,
     timezone = var.timezone
   }
 }
@@ -45,7 +33,7 @@ resource "libvirt_cloudinit_disk" "vm_cloudinit" {
   for_each = var.vm_rockylinux_definitions
 
   name      = "${each.key}_cloudinit.iso"
-  pool      = libvirt_pool.volumetmp_kube_02.name
+  pool      = libvirt_pool.volumetmp.name
   user_data = data.template_file.vm-configs[each.key].rendered
 }
 
@@ -54,7 +42,7 @@ resource "libvirt_volume" "vm_disk" {
 
   name           = "${each.key}-${var.cluster_name}.qcow2"
   base_volume_id = libvirt_volume.rocky9_image.id
-  pool           = libvirt_pool.volumetmp_kube_02.name
+  pool           = libvirt_pool.volumetmp.name
   format         = "qcow2"
 }
 
@@ -86,4 +74,3 @@ resource "libvirt_domain" "vm" {
     mode = "host-passthrough"
   }
 }
-
--- nat_network_03/main.tf
+++ nat_network_03/main.tf
@@ -1,53 +1,33 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    libvirt = {
-      source  = "dmacvicar/libvirt"
-      version = "0.7.0"
-    }
-    ct = {
-      source  = "poseidon/ct"
-      version = "0.10.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2.0"
-    }
-  }
-}
-
-provider "libvirt" {
-  uri = "qemu:///system"
-}
-
+# nat_network_03/main.tf
 resource "libvirt_network" "kube_network_03" {
-  name      = var.rocky9_network_name
+  name      = "kube_network_03"
   mode      = "nat"
   addresses = ["10.17.4.0/24"]
 }
 
-resource "libvirt_pool" "volumetmp_kube_03" {
-  name = "${var.cluster_name}_kube_03"
+resource "libvirt_pool" "volumetmp" {
+  name = var.cluster_name
   type = "dir"
-  path = "/var/lib/libvirt/images/${var.cluster_name}_kube_03"
+  path = "/var/lib/libvirt/images/${var.cluster_name}"
 }
 
 resource "libvirt_volume" "base" {
-  name   = "${var.cluster_name}_kube_03-base"
+  name   = "${var.cluster_name}-base"
   source = var.base_image
-  pool   = libvirt_pool.volumetmp_kube_03.name
+  pool   = libvirt_pool.volumetmp.name
   format = "qcow2"
 }
 
 data "template_file" "vm-configs" {
   for_each = var.vm_definitions
 
-  template = file("${path.module}/config/machine-${each.key}-config.yaml.tmpl")
+  template = file("${path.module}/configs/machine-${each.key}-config.yaml.tmpl")
+
   vars = {
-    ssh_keys     = jsonencode(var.ssh_keys)
-    name         = each.key
-    host_name    = "${each.key}.${var.cluster_name}.${var.cluster_domain}"
-    strict       = true
+    ssh_keys     = jsonencode(var.ssh_keys),
+    name         = each.key,
+    host_name    = "${each.key}.${var.cluster_name}.${var.cluster_domain}",
+    strict       = true,
     pretty_print = true
   }
 }
@@ -62,7 +42,7 @@ resource "libvirt_ignition" "ignition" {
   for_each = var.vm_definitions
 
   name    = "${each.key}-ignition"
-  pool    = libvirt_pool.volumetmp_kube_03.name
+  pool    = libvirt_pool.volumetmp.name
   content = data.ct_config.vm-ignitions[each.key].rendered
 }
 
@@ -71,7 +51,7 @@ resource "libvirt_volume" "vm_disk" {
 
   name           = "${each.key}-${var.cluster_name}.qcow2"
   base_volume_id = libvirt_volume.base.id
-  pool           = libvirt_pool.volumetmp_kube_03.name
+  pool           = libvirt_pool.volumetmp.name
   format         = "qcow2"
 }
 
