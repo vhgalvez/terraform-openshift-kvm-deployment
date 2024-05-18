b8df1fdf20d792e4aff42214bec01408a46e5a47
Victor Galvez <vhgalvez@gmail.com>
2024-05-19 01:55:10 +0200

    update

--- nat_network_02/tf/outputs_01.tf
+++
@@ -1,8 +0,0 @@
-# output.tf
-
-output "ip_addresses" {
-  value = { 
-    for key, vm in libvirt_domain.vm : 
-      key => length(vm.network_interface[0].addresses) > 0 ? vm.network_interface[0].addresses[0] : "No IP Assigned"
-  }
-}
